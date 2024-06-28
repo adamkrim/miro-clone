6fd42344f965f365f1bb3f042539ad7dde834cc1
Adam <adamkrim.dev@gmail.com>
2024-06-28 09:49:30 +0200

    pro modal and fix search boards

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useSearchParams } from "next/navigation";
 import { useOrganization } from "@clerk/nextjs";
 
 import { BoardList } from "./_components/board-list";
@@ -12,15 +13,20 @@ interface DashboardPageProps {
   };
 }
 
-const DashboardPage = ({ searchParams }: DashboardPageProps) => {
+const DashboardPage = () => {
   const { organization } = useOrganization();
+  const searchParams = useSearchParams();
+  const query = {
+    search: searchParams.get("search") || undefined,
+    favorites: searchParams.get("favorites") || undefined,
+  };
 
   return (
     <div className="h-[calc(100%-80px)] flex-1 p-6">
       {!organization ? (
         <EmptyOrg />
       ) : (
-        <BoardList orgId={organization.id} query={searchParams} />
+        <BoardList orgId={organization.id} query={query} />
       )}
     </div>
   );
--- modals/pro-modal.tsx
+++ modals/pro-modal.tsx
@@ -1,6 +1,11 @@
 "use client";
 
+import { Poppins } from "next/font/google";
+import Image from "next/image";
+
+import { cn } from "@/lib/utils";
 import { useProModal } from "@/store/use-pro-modal";
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogClose,
@@ -11,6 +16,11 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
 export const ProModal = () => {
   const { isOpen, onOpen, onClose } = useProModal();
 
@@ -18,9 +28,17 @@ export const ProModal = () => {
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Pro modal</DialogTitle>
-          <DialogDescription>This is a pro modal</DialogDescription>
+          <DialogTitle>🚀 Upgrade to pro</DialogTitle>
+          <DialogDescription>
+            Unlock all the features and take your productivity to the next level
+          </DialogDescription>
+          <Image src="/pro.svg" alt="Pro" height={200} width={200} />
         </DialogHeader>
+        <DialogFooter>
+          <Button size="sm" className="w-full">
+            Upgrade
+          </Button>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
