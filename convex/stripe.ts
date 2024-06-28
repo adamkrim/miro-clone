79e2c7840aa85740e92a27436360d6632f8dc7f2
Adam <adamkrim.dev@gmail.com>
2024-06-28 10:38:46 +0200

    stripe webhook

--- convex/stripe.ts
+++ convex/stripe.ts
@@ -3,7 +3,7 @@
 import { v } from "convex/values";
 import Stripe from "stripe";
 
-import { action } from "./_generated/server";
+import { action, internalAction } from "./_generated/server";
 
 const url = process.env.NEXT_PUBLIC_APP_URL!;
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
@@ -50,5 +50,34 @@ export const pay = action({
     });
 
     return session.url!;
+  },
+});
+
+export const fulfill = internalAction({
+  args: {
+    signature: v.string(),
+    payload: v.string(),
+  },
+  handler(ctx, { signature, payload }) {
+    const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!;
+
+    try {
+      const event = stripe.webhooks.constructEvent(
+        payload,
+        signature,
+        webhookSecret
+      );
+
+      const session = event.data.object as Stripe.Checkout.Session;
+
+      if (event.type === "checkout.session.completed") {
+        console.log("CHECKOUT_COMPLETED!");
+      }
+
+      return { success: true };
+    } catch (error) {
+      console.error(error);
+      return { success: false };
+    }
   },
 });
